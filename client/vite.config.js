78e29d18bf803712830c1d1af4a4949550380392
AryaJeet1364 <aryajeet.jha@gmail.com>
2026-06-24 10:32:25 +0530

    updated syntax

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,9 +17,9 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          socket: ['socket.io-client']
+        manualChunks: (id) => {
+          if (id.includes('socket.io-client')) return 'socket';
+          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) return 'vendor';
         }
       }
     }
